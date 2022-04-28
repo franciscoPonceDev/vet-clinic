59d8582e27ba6d555a153df54d9d4f09d22ab542
Francisco Ponce <francisco.ponce.dev@gmail.com>
2022-04-27 22:17:01 -0300

    create table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,12 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
+CREATE TABLE animals
+(
+    id integer NOT NULL,
+    name character varying(100) NOT NULL,
+    date_of_birth date,
+    escape_attempts integer DEFAULT 0,
+    neutered boolean,
+    weight_kg numeric,
+    PRIMARY KEY (id)
 );
