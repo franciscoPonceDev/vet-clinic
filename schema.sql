6b731b267eb31739dc4b6e7dc9967a500f521669
Estebanmual <esteban.munozal@gmail.com>
2022-05-02 16:43:05 -0500

    Add index and Explain Analyze for owners

--- schema.sql
+++ schema.sql
@@ -54,3 +54,6 @@ EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animal_id = 4;
 CREATE INDEX visits_vets_index ON visitis(vet_id)
 EXPLAIN ANALYZE SELECT * FROM visits where vet_id = 2;
 
+CREATE INDEX owners_email_index ON owners(email);
+EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
+
